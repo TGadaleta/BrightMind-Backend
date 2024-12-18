73b9320828851280cd71f49ca75b1161c0f8b07d
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-12-18 15:01:47 -0500

    models for MVP created

---
+++ models/user.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+
+const todoSchema = new mongoose.Schema({
+    text:{
+        type: String,
+        required: true
+    },
+    dueDate:{
+        type: String,
+    },
+    isComplete:{
+        type: Boolean,
+        required: true
+    } 
+})
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    hashedPassword:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    courses:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }],
+    todos:[{
+        type: [todoSchema],
+    }]
+})
+
+userSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+      delete returnedObject.hashedPassword;
+    },
+  });
+
+const User = mongoose.model('User', userSchema)
+
+export default User
