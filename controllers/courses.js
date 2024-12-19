1dc9db34d181fa249b0621b52780294057f395a5
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-12-19 13:59:11 -0500

    courses controller working

---
+++ controllers/courses.js
@@ -0,0 +1,27 @@
+import '../models/lesson.js'
+import express from 'express'
+import Course from '../models/course.js'
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const courses = await Course.find({})
+        res.status(200).json(courses)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.get('/:courseId', async (req,res) => {
+    try {
+        const course = await Course.findById(req.params.courseId).populate('lessons')
+        console.log(course)
+        res.status(200).json(course)
+    } catch (error) {
+        res.status(500)
+        console.log(error)
+    }
+})
+
+export default router
