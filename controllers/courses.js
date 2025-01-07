8225ee87e39cb529c309d0902dd3dac04ff2892b
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-07 09:16:18 -0500

    added route to add course, changed course schema to add owner to course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 import '../models/lesson.js'
 import express from 'express'
 import Course from '../models/course.js'
+import verifyToken from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
@@ -20,6 +21,26 @@ router.get('/:courseId', async (req,res) => {
     } catch (error) {
         res.status(500)
         console.log(error)
+    }
+})
+
+router.post('/add', verifyToken, async (req,res) => {
+    try {
+        const newCourse = req.body
+        const courses = await Course.find({})
+        const repeatedCourse = courses.some(course => course.name === newCourse.name)
+        if (repeatedCourse){
+            return res.status(412).json( {message: "This class name already exists. Please pick a new one"} )
+        }
+        const addedCourse = await Course.create(
+            {
+                name: newCourse.name,
+                department: newCourse.department,
+                description: newCourse.description
+            });
+        return res.status(200).json(addedCourse)
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
 
